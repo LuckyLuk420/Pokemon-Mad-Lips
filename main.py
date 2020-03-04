8df4c808ea3d8bcf0aef7261f42e7894f136aec4
luckyluk420 <lpurmom@gmail.com>
2020-03-04 18:06:29 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+lyrics = "I want to be the very {0},\nLike no one ever was.\nTo {1} them is my real test,\nTo {2} them is my {3}!"
+
+adj = input("Please enter a Superlative Adjective or Noun: ")
+ver1 = input("Please enter a Verb: ")
+ver2 = input("Please enter another Verb: ")
+noun = input("Please enter a Noun: ")
+print()
+print()
+print()
+print(lyrics.format(adj.upper(), ver1.upper(), ver2.upper(), noun.upper()))
